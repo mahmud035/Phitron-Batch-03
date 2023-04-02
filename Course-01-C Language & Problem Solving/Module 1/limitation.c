18c74b421eb92ddf5200199b07dfc3b8a23be8d6
mahmud035 <mahamudulhasan4148@gmail.com>
2023-04-02 14:28:23 +0600

    }

---
+++ Course-01-C Language & Problem Solving/Module 1/limitation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  // int a = 1000000000;
+  // long long int b = 10000000000000;
+  // printf("%lld", b);
+
+  float f = 200.4353450354343243243;
+  printf("%f\n", f);
+  double d = 200.4353450354343243243;
+  printf("%lf\n", d);
+  printf("%0.15lf", d);
+
+  return 0;
+}
