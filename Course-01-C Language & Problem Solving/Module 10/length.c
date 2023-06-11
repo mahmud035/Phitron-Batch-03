66459d3c04d8836746d5b53c35024d93cba0f6d2
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-11 19:35:39 +0600

    10-5 Length of a String

--- Course-01-C Language & Problem Solving/Module 10/length.c
+++ Course-01-C Language & Problem Solving/Module 10/length.c
@@ -5,23 +5,8 @@ int main()
   char a[100];
   scanf("%s", a);
 
-  int count = 0;
-  int i;
-
-  // Using for loop
-  for (i = 0; a[i] != '\0'; i++)
-  {
-    count++;
-  }
-  printf("%d\n", count);
-
-  // Using while loop
-  while (a[i] != '\0')
-  {
-    count++;
-    i++;
-  }
-  printf("%d\n", count);
+  int length = strlen(a);
+  printf("%d\n", length);
 
   return 0;
 }
