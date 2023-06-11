8ff36e1995022096d9fafbfa2b046e9a387830a2
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-11 09:37:46 +0600

    practice string from Sobar Jonno C book

---
+++ Course-01-C Language & Problem Solving/Module 10/c_for_everyone.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+  /* char msg[] = "Copyright @ 1997 home computing group";
+  printf("%s", msg); */
+
+  /*   char *city_name[3] = {"Dhaka",
+                          "Khulna",
+                          "Rajshahi"};
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+      printf("\nCity name [%d] is %s.", i + 1, city_name[i]);
+    } */
+
+  /* char name[64];
+  printf("\n Your name please: ");
+  gets(name);
+
+  printf("\n\n So, you are %s\n", name);
+  puts(name); */
+
+  /*   char name_1[30] = "John Doe";
+    char name_2[30];
+
+    strcpy(name_2, name_1);
+    printf("Programmer name is: %s\n", name_2); */
+
+  return 0;
+}
