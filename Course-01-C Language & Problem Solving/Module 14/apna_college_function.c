25c3a196cb9c31f92fcdb1033b64f5a83b1674ca
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-19 09:17:51 +0600

    basic function syntax

---
+++ Course-01-C Language & Problem Solving/Module 14/apna_college_function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// function definition/prototype
+void printHello(void);
+
+int main()
+{
+
+  // function calling / invoking
+  printHello();
+
+  return 0;
+}
+
+// function declaration
+void printHello()
+{
+  printf("Hello!");
+}
