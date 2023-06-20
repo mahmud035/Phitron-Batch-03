c74034811295eb2060a13c88cdf856d75650c20d
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-20 08:18:50 +0600

    14-2 Return + Parameter

--- Course-01-C Language & Problem Solving/Module 14/apna_college_pointers.c
+++ Course-01-C Language & Problem Solving/Module 14/apna_college_pointers.c
@@ -2,22 +2,28 @@
 
 int main()
 {
-  int x;
-  int *ptr;
+  // int x;
+  // int *ptr;
 
-  ptr = &x;
-  *ptr = 0; // x = 0
+  // ptr = &x;
+  // *ptr = 0; // x = 0
 
-  printf("x = %d\n", x);
-  printf("*ptr = %d\n", *ptr);
+  // printf("x = %d\n", x);
+  // printf("*ptr = %d\n", *ptr);
 
-  *ptr += 5;
-  printf("x = %d\n", x);
-  printf("*ptr = %d\n", *ptr);
+  // *ptr += 5;
+  // printf("x = %d\n", x);
+  // printf("*ptr = %d\n", *ptr);
 
-  (*ptr)++;
-  printf("x = %d\n", x);
-  printf("*ptr = %d\n", *ptr);
+  // (*ptr)++;
+  // printf("x = %d\n", x);
+  // printf("*ptr = %d\n", *ptr);
+
+  int i = 5;
+  int *ptr = &i;
+  int **pptr = &ptr;
+
+  printf("%d\n", **pptr);
 
   return 0;
 }
