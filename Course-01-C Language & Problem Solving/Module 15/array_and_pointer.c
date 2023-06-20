e26286e57f779392f378fa9f2a77ed208c700fb5
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-20 19:57:16 +0600

    15-6 Function and String

--- Course-01-C Language & Problem Solving/Module 15/array_and_pointer.c
+++ Course-01-C Language & Problem Solving/Module 15/array_and_pointer.c
@@ -3,8 +3,8 @@
 int main()
 {
   int arr[5] = {10, 20, 30, 40, 50};
-  // printf("0th index er address - %p\n", &arr[0]);
-  // printf("0th index er address - %p\n", arr);
+  printf("0th index er address - %p\n", &arr[0]);
+  printf("0th index er address - %p\n", arr);
 
   // printf("0th index er value - %d\n", arr[0]);
   // printf("0th index er value - %d\n", *arr);
@@ -15,10 +15,10 @@ int main()
   // printf("2nd index er value - %d\n", arr[2]);
   // printf("2nd index er value - %d\n", *(arr + 2));
 
-  printf("%d\n", *(arr + 1));
-  printf("%d\n", *(1 + arr));
-  printf("%d\n", arr[1]);
-  printf("%d\n", 1 [arr]);
+  // printf("%d\n", *(arr + 1));
+  // printf("%d\n", *(1 + arr));
+  // printf("%d\n", arr[1]);
+  // printf("%d\n", 1 [arr]);
 
   return 0;
 }
