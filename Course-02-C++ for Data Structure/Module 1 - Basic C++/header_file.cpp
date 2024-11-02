dc95f137ba8e4f9872e281f0c29c790ee83eb48c
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-02 21:35:05 +0600

    create cpp snippet

--- Course-02-C++ for Data Structure/Module 1 - Basic C++/header_file.cpp
+++ Course-02-C++ for Data Structure/Module 1 - Basic C++/header_file.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 int main()
 {
-  int a, b;
-  cin >> a >> b;
-  cout << a << " " << b;
-
+  
   return 0;
 }
--- Course-02-C++ for Data Structure/Module 1 - Basic C++/switch_statement.cpp
+++ Course-02-C++ for Data Structure/Module 1 - Basic C++/switch_statement.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-  int day = 1;
+  int day = 4;
 
   switch(day){
     case 1:
